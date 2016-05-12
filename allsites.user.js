c01dcb8846037901af08e8afd0e2eec86887bd55
yweba <yweb@mail.ru>
2016-05-12 09:06:09 +0600

    init
    init

---
+++ allsites.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name          All sites
+// @namespace     all_sites
+// @description	  All sites
+// @include       *
+// @version       00.00.01
+// @downloadURL   https://github.com/yweba/yweba/allsites.user.js
+// @updateURL     https://github.com/yweba/yweba/allsites.user.js
+// @grant         GM_log
+// @grant         GM_xmlhttpRequest
+// ==/UserScript==
+
+(function(){
+  console.log("test 1");
+})();
