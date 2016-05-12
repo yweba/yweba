cab6b9d9d655593a71d122123d647efa843de686
yweba <yweb@mail.ru>
2016-05-12 09:10:14 +0600

    update
    update

--- allsites.user.js
+++ allsites.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name          All sites
+// @name          All sites 00.00.04
 // @namespace     all_sites
-// @description	  All sites
+// @description	  All sites 00.00.04
 // @include       *
-// @version       00.00.03
+// @version       00.00.04
 // @downloadURL   https://github.com/yweba/yweba/raw/master/allsites.user.js
 // @updateURL     https://github.com/yweba/yweba/raw/master/allsites.user.js
 // @grant         GM_log
@@ -11,5 +11,5 @@
 // ==/UserScript==
 
 (function(){
-  console.log("test 3");
+  console.log("test 4");
 })();
